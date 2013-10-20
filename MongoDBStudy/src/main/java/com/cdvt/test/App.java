934a461473376dc5c5c3c4dd31c4778b0f71a416
chenjian <cj520025@gmail.com>
2013-10-20 15:49:38 +0800

    tijc

---
+++ MongoDBStudy/src/main/java/com/cdvt/test/App.java
@@ -0,0 +1,20 @@
+package com.cdvt.test;
+
+/**
+ * Hello world!
+ *
+ */
+ import com.mongodb.MongoClient;
+public class App 
+{
+    public static void main( String[] args )
+    {
+		try
+		{
+        MongoClient mc = new MongoClient();
+		}
+		catch (Exception e)
+		{
+		}
+    }
+}
